4f91a1e6ef90b333a5993518b5ba0813b7991515
Krazy0007 <115631543+Krazy0007@users.noreply.github.com>
2022-11-19 11:56:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -651,11 +651,11 @@ async def auto_filter(client, msg, spoll=False):
             [
                 InlineKeyboardButton(
                     text=f"{file.file_name}",
-                    callback_data=f'{pre}#{file.file_id}',
+                    url=f'{pre}#{file.file_id}',
                 ),
                 InlineKeyboardButton(
                     text=f"{get_size(file.file_size)}",
-                    callback_data=f'{pre}#{file.file_id}',
+                    url=f'{pre}#{file.file_id}',
                 ),
             ]
             for file in files
